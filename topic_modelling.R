522fed16d3c525f6d79e9bbfa5fb8f520c7d9dca
khaled-mansour <khaled-mansour@live.fr>
2018-01-09 23:47:04 +0100

    Update topic_modelling.R

--- topic_modelling.R
+++ topic_modelling.R
@@ -1,37 +1,32 @@
 ####################### Part 4:  topic modelling ########################
 library(tm)
-# We will work with the term-document matrix that we have
-# created for the 2000 Yelp reviews
 reviews <- readRDS("./yelp_dtm.rds")
-
-# Have a quick look at reviews to see if everything worked
 inspect(reviews[1:5, 1:10])
 
-# Number of empty documents in the matrix
-rowTotals <- apply(reviews, 1, sum) #Find the sum of words in each Document
-sum(rowTotals == 0) # There is one review in the corpus that doesn't contain any freq. term
+# Nombre de documents vides dans la matrice
+rowTotals <- apply(reviews, 1, sum) #Trouver la somme des mots dans chaque document
+sum(rowTotals == 0) # Il y a une revue dans le corpus qui ne contient aucune freq. terme
 reviews   <- reviews[rowTotals > 0, ]  
 
 install.packages("topicmodels")
 library(topicmodels)
-# fitting the model
+# adapter le modèle
 k <- 20
 fit <- LDA(reviews, k, method = "Gibbs", control = list(seed = 123, verbose = 250, burnin = 500, iter = 500))
 
-# results
+# resultats
 terms(fit, 10)
 topics(fit)[1:10]
 ldaOut.topics <- as.matrix(topics(fit))
 
 
-## Additional functionality
-There are two additional packages that provide convenient helper functions. **ldatuning** helps us to find a good number of topics to describe the data. **LDAvis** provides a dynamic interface to explore the topics.
+## Fonctionnalité supplémentaire
+# Il y a deux paquets supplémentaires qui fournissent des fonctions auxiliaires pratiques. ** Ldatuning ** nous aide à trouver un bon nombre de sujets pour décrire les données. ** LDAvis ** fournit une interface dynamique pour explorer les sujets.
 
-```{r}
 #### Topic models
 install.packages("ldatuning")
 library(ldatuning)
-# ldatuning to find optimal topics number
+# ldatuning pour trouver le nombre optimal des sujets "topics"
 kTuning <- FindTopicsNumber(
   reviews,
   topics = seq(from = 16, to = 24, by = 4),
@@ -43,20 +38,20 @@ kTuning <- FindTopicsNumber(
 )
 FindTopicsNumber_plot(kTuning)
 
-#### Visualization of topics
+#### Visualisation des "topics"
 install.packages("LDAvis")
 library(LDAvis)
 
-# Find required quantities
-phi <- as.matrix(posterior(fit)$terms) # The topic-word mixture
-theta <- as.matrix(posterior(fit)$topics) # The document-topic mixture
+# Trouver les quantités requises
+phi <- as.matrix(posterior(fit)$terms) # Le mélange sujet-mot
+theta <- as.matrix(posterior(fit)$topics) # Le mélange document-sujet
 
-# Convert to json to display dynamically with shiny
+# Convertir en json pour afficher dynamiquement avec Shiny
 fit_json <- LDAvis::createJSON(phi = phi, theta = theta,
-                               vocab = colnames(phi), # The unique terms
-                               doc.length = rowSums(as.matrix(reviews)), # The number of words in each document
-                               term.frequency = colSums(as.matrix(reviews)) # The frequency of each term overall
+                               vocab = colnames(phi), # les terms uniques
+                               doc.length = rowSums(as.matrix(reviews)),# Le nombre de mots dans chaque document
+                               term.frequency = colSums(as.matrix(reviews)) # La fréquence global de chaque terme
 )
 
-# Load json with shiny
+# Lancer json avec shiny
 serVis(fit_json)
